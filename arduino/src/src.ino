bed6e9c57b18dd1d858c548b49dc967e7ba7bdbb
Baptiste Labat <baptiste.labat@gmail.com>
2014-03-06 00:26:21 +0100

    Many changes not saved in previous commit

--- arduino/src/src.ino
+++ arduino/src/src.ino
@@ -8,9 +8,8 @@
  
  The circuit:
   - a water level sensor http://composants.e44.com/capteurs/capteurs-niveau-d-eau/capteur-niveau-eau-normalement-ouvert-SCR4.html is linking 0V to analog pin A0 (with a 10kohm pull up resistor to 5V)
-  - an electromechanical relay commanding a pump is connected to digital output pin 4
-  - an electromechanical relay commanding an electrovanne is connected to pin 3
- * LED connected from digital pin 9 to ground
+  - an electromechanical relay commanding a 12V pump is connected to digital output pin 4 (and to ground and 5V)
+  - an electromechanical relay commanding an 220V electrovanne is connected to pin 3 (and to ground and 5V)
  
  Program by Baptiste LABAT, 2013
  
@@ -19,7 +18,7 @@
 
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int analogOutPin = 3; // Analog output pin that the LED is attached to
+const int valvePin = 3; // Analog output pin that the LED is attached to
 const int analogInPin = A0;
 const int pumpPin = 4;
 const int ledPin = 13;
@@ -31,16 +30,17 @@ String inputString = "";         // A string to hold incoming data
 boolean stringComplete = false;  // Whether the string is complete
 int outputValue = 0;
 float receivedValue = 0.0;
+int sensorMax = 1024;
 
-boolean levelWasDown = true;
-boolean levelIsDown = true;
+boolean waterLevelWasUp = true;
+boolean waterLevelIsUp = true;
 boolean freeze = false;
 
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
     pinMode(pumpPin, OUTPUT); 
-    pinMode(analogOutPin, OUTPUT); 
+    pinMode(valvePin, OUTPUT); 
 }
 
 void serialEvent() {
@@ -68,23 +68,22 @@ float StrToFloat(String str){
 }
 
 void loop() {    
+  Serial.print("dropCounter: ");
+  Serial.println(dropCounter);
+  managePump();
+  makeADrop();
+}
 
-  // print the results to the serial monitor:
-        
-
-  // wait 2 milliseconds before the next loop
-  // for the analog-to-digital converter to settle
-  // after the last reading:
+void managePump() {
   sensorValue = analogRead(analogInPin);
   Serial.print("Sensor: ");
   Serial.println(sensorValue);
-  levelIsDown = sensorValue>512;
-  Serial.print("dropCounter: ");
-  Serial.println(dropCounter);
   
-  if (dropCounter>150) //This should normaly never happen
+  waterLevelIsUp = sensorValue>sensorMax/2;
+  
+  if (dropCounter>150) //This should normaly never happen, the sensor should start the pump before
   {
-    if (levelIsDown)
+    if (waterLevelIsUp)
     { //Too many drops without sensor activation: sensor is not working, stop everything!
       freeze = true;
     }
@@ -94,18 +93,19 @@ void loop() {
     }
   }
   
-  if (!levelIsDown) //It is time to activate the pump
+  if (!waterLevelIsUp) //Not enough water, it is time to activate the pump
   {
     
-    if (levelWasDown) //normal case, the level was previously low
+    if (waterLevelWasUp) //normal case, the level was previously up
     { 
       //Active the pump for a few seconds (if not freezed)
       if (!freeze)
       {
         digitalWrite(pumpPin, HIGH);
-        Serial.println("Pump");
+        Serial.println("Pumping");
         delay(1000);
         digitalWrite(pumpPin, LOW);
+        Serial.println("Stop pumping");
         delay(2000);
         dropCounter = 0; //Reset the drop counter
       }
@@ -119,22 +119,23 @@ void loop() {
   }
   else
   {
-    if (!levelWasDown) // The level was previously up, the sensor is working
+    if (!waterLevelWasUp) // The level was previously up, the sensor is working
     {
       freeze = false;
     }
     digitalWrite(pumpPin, LOW);
   }
-  levelWasDown = (sensorValue>512);
+  waterLevelWasUp = waterLevelIsUp;
+}
 
-  // change the analog out value:
-  analogWrite(analogOutPin, int(receivedValue));
+void makeADrop() {
+  /* Map the drop frequency to the precipitation (0-0.4inch/hr normalised to 0-255)*/
   Serial.println(int(receivedValue));
   if (receivedValue < 1)
   {
       Serial.println("No rain");
       digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-      digitalWrite(analogOutPin, LOW);    // turn the LED off by making the voltage LOW
+      digitalWrite(valvePin, LOW);    // turn the LED off by making the voltage LOW
       delay(1000);               // wait for a second
   }
   else
@@ -165,15 +166,18 @@ void loop() {
        Serial.print("Delay: ");
        Serial.println(delayValue);
        digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-       digitalWrite(analogOutPin, LOW);    // turn the LED off by making the voltage LOW
+       digitalWrite(valvePin, LOW);    // turn the valve off by making the voltage LOW
        delay(delayValue);         // wait for a second
        digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-       if (!freeze) //only if not problem
+       if (!freeze) //only if no problem
        {
-         digitalWrite(analogOutPin, HIGH);   // turn the flow on (HIGH is the voltage level)
+         digitalWrite(valvePin, HIGH);   // turn the flow on (HIGH is the voltage level)
          dropCounter++;
        }
        delay(1100);               // wait for a second
+       
+       digitalWrite(ledPin, LOW);
+       digitalWrite(valvePin, LOW); 
        
    }
 }
